bf5710e0bdbcd244808f6208849f259b196141f1
NhanTVu <uvnahn21@gmail.com>
2018-01-29 17:16:12 -0600

    add dice roll function

--- world.hpp
+++ world.hpp
@@ -10,6 +10,15 @@ public:
 		std::cout<<"-called from world_builder"<<'\n';
 	}
 
+	bool roll(int side, int success, double modifier){
+		srand(time(NULL));			//random seed
+		int roll = rand()%side+1; 	//return a num 1-side
+		roll*= modifier; 			//mult result *modifier
+		if(roll < success){ return false;}
+		else{ return true;}
+	}
+
+
 private:
 	std::vector<item*> all_items;
 	std::vector<character*> all_characters;
